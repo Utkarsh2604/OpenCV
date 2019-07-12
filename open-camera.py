e7de04151a8ff7c801f8b951d2df8484c597cf81
Utkarsh2604 <nagpureutkarsh@gmail.com>
2019-07-12 22:39:02 +0530

    Opened Camera

--- open-camera.py
+++ open-camera.py
@@ -1,14 +1,15 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) # Using Camera
 
 while(True):
 
-    ret, frame = cap.read()
+    ret, frame = cap.read() #Reading Frame
 
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
+    cv2.imshow('frame',frame)#Showing Frame
+
+    if cv2.waitKey(20) & 0xFF == ord('q'):#Loop To Continue Showing Output
         break
 
 cap.release()
