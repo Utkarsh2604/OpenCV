762aee49a0fe0d4135245e6cc02896c57b5ffa38
Utkarsh2604 <nagpureutkarsh@gmail.com>
2019-07-12 23:42:23 +0530

    B&W Image out

---
+++ B&W-out.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0) # Using Camera
+
+while(True):
+
+    ret, frame = cap.read() #Reading Frame
+
+    bw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)#B&W frame
+
+
+    cv2.imshow('frame',frame)#Showing Frame
+
+    cv2.imshow('frame1',bw)#Showing B&w Frame
+
+
+    if cv2.waitKey(20) & 0xFF == ord('C'):#Loop To Continue Showing Output
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+"""Use 'C' Button to Quit"""
