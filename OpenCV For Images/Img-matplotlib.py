164b2053caabe3e0b19d9eead5236317e9514d69
Utkarsh Nagpure <41725470+Utkarsh2604@users.noreply.github.com>
2019-07-14 01:55:22 +0530

    Matplotlib

--- OpenCV For Images/Img-matplotlib.py
+++ OpenCV For Images/Img-matplotlib.py
@@ -4,10 +4,8 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('goku.jpg',1)#import image
 
-
 img = img[:,:,::-1]#to show image in rgb
 
-
-plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')#matplotlib 
 plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
 plt.show()
