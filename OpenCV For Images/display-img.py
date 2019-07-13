2c0fb514d687ee19e49d5e08ac9378cca4437a32
Utkarsh Nagpure <41725470+Utkarsh2604@users.noreply.github.com>
2019-07-14 01:32:59 +0530

    Update display-img.py

--- OpenCV For Images/display-img.py
+++ OpenCV For Images/display-img.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('goku.jpg', 1)   #  imporing image
+img = cv2.imread('goku.jpg', 1)   #  importing image
 
 cv2.imshow('image',img)  #showing images 
 
