4d579712950280a99cedaf49085940e2024da0c5
Utkarsh Nagpure <41725470+Utkarsh2604@users.noreply.github.com>
2019-07-14 01:53:09 +0530

    B&W Output Video
    b&W

--- B&W-out.py
+++ B&W-out.py
@@ -6,14 +6,12 @@ cap = cv2.VideoCapture(0) # Using Camera
 while(True):
 
     ret, frame = cap.read() #Reading Frame
-
+    
     bw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)#B&W frame
-
 
     cv2.imshow('frame',frame)#Showing Frame
 
     cv2.imshow('frame1',bw)#Showing B&w Frame
-
 
     if cv2.waitKey(20) & 0xFF == ord('x'):#Loop To Continue Showing Output
         break
@@ -22,3 +20,4 @@ cap.release()
 cv2.destroyAllWindows()
 
 """Use 'x' Button to Quit"""
+
