5d7789b7fc2e2594f8a4f4d3bd517deb0ea4c002
Utkarsh Nagpure <41725470+Utkarsh2604@users.noreply.github.com>
2019-07-14 01:54:16 +0530

    Displaying Image

--- OpenCV For Images/display-img.py
+++ OpenCV For Images/display-img.py
@@ -1,9 +1,9 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('goku.jpg', 1)   #  importing image
+img = cv2.imread('goku.jpg', 1)   #importing image
 
-cv2.imshow('image',img)  #showing images 
+cv2.imshow('image',img)  #Showing images 
 
 k = cv2.waitKey(0) 
 
